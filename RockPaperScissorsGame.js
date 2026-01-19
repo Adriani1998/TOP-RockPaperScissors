b1ae1facd1abed2b05426fae23704bcf5ef62ce0
Adriani <104438899+Adriani9819@users.noreply.github.com>
2026-01-19 19:58:43 +0100

    Create function: Computer game selection

--- RockPaperScissorsGame.js
+++ RockPaperScissorsGame.js
@@ -1,5 +1,13 @@
 /* Functions section */
 
+function getComputerChoice () {
+
+    const gameOptions = ["Rock","Paper","Scissors"];
+    let randomNum = Math.round(3 * Math.random());
+
+    return gameOptions[randomNum];
+}
 
 /* Game Section */
 
+console.log(getComputerChoice());

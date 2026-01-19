e6e0595423c11540a031d438a290124e3746a7ec
Adriani <104438899+Adriani9819@users.noreply.github.com>
2026-01-19 20:09:00 +0100

    Create function: Human game selection

--- RockPaperScissorsGame.js
+++ RockPaperScissorsGame.js
@@ -3,11 +3,20 @@
 function getComputerChoice () {
 
     const gameOptions = ["Rock","Paper","Scissors"];
-    let randomNum = Math.round(3 * Math.random());
+    let randomNum = Math.round(2 * Math.random());
 
     return gameOptions[randomNum];
+}
+
+function getHumanChoice () {
+
+    const gameOptions = ["Rock","Paper","Scissors"];
+    let humanChoice = window.prompt("Write 0(Rock), 1(Paper) or 2(Scissors)",)
+
+    return gameOptions[humanChoice];
 }
 
 /* Game Section */
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
